2216a6322b6d8852fe1305c15f1ba6b2cacc17b0
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-11 21:49:42 -0400

    create middleware dir + build logger.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-const morgan = require('morgan');
+const logger = require('./middleware/logger');
 const postRouter = require('./posts/postRouter');
 
 const server = express();
@@ -11,49 +11,30 @@ server.use(express.json())
 
 // 3rd party middleware
 server.use(helmet());
-// server.use(morgan('dev'));
 
 // custom middleware
-server.use(logger);
-server.use(validateUser);
+server.use(logger('short'));
+// server.use(validateUser);
 server.use(postRouter);
 
-// server.get('/', (req, res) => {
-//   res.send(`<h2>Let's write some middleware!</h2>`);
-// });
 
-//custom middleware
-
-function logger(req, res, next) {
-  console.log(`${req.method} method, ${req.url} req url, ${Date.now()}`);
-  next();
-}
-
-// function validateUserId(req, res, next) {
-//   if (req.params.id) {
-//     req.user = req
-//   } else {
+// function validateUser(req, res, next) {
+//   if (!req.body) {
 //     res.status(400).json({
-//       message: "invalid user id"
+//       message: "missing user data"
+//     })
+//   } else if (!req.body.name) {
+//     res.status(400).json({
+//       message: "missing required name field"
 //     })
 //   }
 
 //   next();
 // }
 
-function validateUser(req, res, next) {
-  if (!req.body) {
-    res.status(400).json({
-      message: "missing user data"
-    })
-  } else if (!req.body.name) {
-    res.status(400).json({
-      message: "missing required name field"
-    })
-  }
-  
-  next();
-}
+// server.get('/', (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
 
 server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
