768b4aeb976cf468b9c6fc0c1e110e9d22db3684
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-11 21:01:33 -0400

    build custom logger middleware to run on every request made to the API + edit readme

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const postRouter = require('./posts/postRouter');
 
 const server = express();
@@ -7,6 +8,8 @@ const PORT = 5000;
 
 server.use(express.json())
 server.use(helmet());
+// server.use(morgan('dev'));
+server.use(logger);
 server.use(postRouter);
 
 // server.get('/', (req, res) => {
@@ -16,7 +19,8 @@ server.use(postRouter);
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} method, ${req.url} req url, ${Date.now()}`);
+  next();
 }
 
 server.listen(PORT, () => {
