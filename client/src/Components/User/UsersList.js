b864d0b946289b47a8ee40f9c15db78036c82148
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-21 16:02:33 -0400

    add response header to handle CORS error

--- client/src/Components/User/UsersList.js
+++ client/src/Components/User/UsersList.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 export default function UsersList() {
+  useEffect(() => {
+    axios.get('http://localhost:5000')
+      .then(response => {
+        console.log(response)
+      })
+  }, [])
+
   return (
     <div>
       <h2>List of Users</h2>
